dabb68df73f7fee98145b2763a1d00b9a8cd5adb
DevNetwork™️ <DevNetworkYtb@outlook.fr>
2022-01-09 15:03:10 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -28,7 +28,7 @@ debian() {
     message "VPS : Debian | Installateur de VPS par \e[34mDevNetwork#2103\e[39m", "Installation des dépendances BASIC effectuer, Création de l'environnement pour le programme !"
     mkdir /home/InstallerVps/
     cd /home/InstallerVps/
-    echo "$(date '+ %H:%M') : Environnement en cours d'installation !" >> logs.txt
+    echo "$(date '+ %d/%m/%Y') | $(date '+ %H:%M') > Environnement en cours d'installation !" >> logs.txt
     # mkdir IV_dataSave
     # echo "$jour | $heure : Dossier [IV_dataSave] créer par le programme ($USER)!" >> logs.txt
     # mkdir IV_tempData
